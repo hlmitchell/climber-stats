1acb72f220c8fc75767a447e4f07e46df97962fb
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-11-06 18:14:06 -0800

    frontend basics setup with react

---
+++ src/client/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <div>HEY</div>
+    )
+  }
+}
+
+export default App;
