5f3717c27b59904fa47103aabe20bdd6fca69aa3
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-11-16 15:09:26 -0800

    added test to /deleteAccount with wrong jwt

--- src/server/__test__/signupRoute.test.js
+++ src/server/__test__/signupRoute.test.js
@@ -1,50 +1,70 @@
 const request = require('supertest');
 const { app, server, mongoose } = require('../index.js');
 
-describe('Test the /signup route', () => {
+describe('Test user routes', () => {
+  describe('Test /signup route', () => {
+    test('respond with 200 if new user', async () => {
+      const response = await request(app)
+        .post('/signup')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'codesmith', 
+          password: 'ilovetesting'
+        })
+      expect(response.statusCode).toBe(200);
+    });
 
-  test('respond with 200 if new user', async () => {
-    const response = await request(app)
-      .post('/signup')
-      .set('Accept', 'application/json')
-      .send({
-        username: 'codesmith', 
-        password: 'ilovetesting'
-      })
-    expect(response.statusCode).toBe(200);
+    test('respond with 403 if user already exists', async () => {
+      const response = await request(app)
+        .post('/signup')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'codesmith', 
+          password: 'ilovetesting'
+        })
+      expect(response.statusCode).toBe(403);
+    });
+
+    test('respond with 400 if wrong body sent', async () => {
+      const response = await request(app)
+        .post('/signup')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'wrong' 
+        })
+      expect(response.statusCode).toBe(400);
+    });
   });
+  
+  // describe('test /login route', () => {
 
-  test('respond with 403 if user already exists', async () => {
-    const response = await request(app)
-      .post('/signup')
+  // })
+
+  describe('test /deleteAccount route', () => {
+
+    test('try to delete user with incorrect jwt', async () => {
+      const key = 'Bearer wrongJwt';
+      const response = await request(app)
+      .delete('/deleteAccount')
       .set('Accept', 'application/json')
+      .set('Authorization', key)
       .send({
-        username: 'codesmith', 
-        password: 'ilovetesting'
+        username: "codesmith"
       })
-    expect(response.statusCode).toBe(403);
-  });
-
-  test('respond with 400 if wrong body sent', async () => {
-    const response = await request(app)
-      .post('/signup')
-      .set('Accept', 'application/json')
-      .send({
-        username: 'wrong' 
-      })
-    expect(response.statusCode).toBe(400);
-  });
-
-  test('delete the user', async () => {
-    const key = `Bearer ${process.env.TEST_JWT}`;
-    const response = await request(app)
-    .delete('/deleteAccount')
-    .set('Accept', 'application/json')
-    .set('Authorization', key)
-    .send({
-      username: "codesmith"
+      expect(response.statusCode).toBe(403);
     })
-    expect(response.statusCode).toBe(200);
+
+    test('delete the user', async () => {
+      const key = `Bearer ${process.env.TEST_JWT}`;
+      const response = await request(app)
+      .delete('/deleteAccount')
+      .set('Accept', 'application/json')
+      .set('Authorization', key)
+      .send({
+        username: "codesmith"
+      })
+      expect(response.statusCode).toBe(200);
+    })
   })
 
   afterAll((done) => {
