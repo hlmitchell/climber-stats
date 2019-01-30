a42e09ec14aa66617cfefc4481f62ca63beda56c
Hannah Mitchell <hannah@hannahs-mbp-2.home>
2019-01-30 12:39:01 -0800

    need to see if I can get around having to put dummy tests in route files

--- __tests__/server/allRoutes.test.js
+++ __tests__/server/allRoutes.test.js
@@ -1,14 +1,19 @@
 const request = require('supertest');
 const { app, server, mongoose } = require('../../src/server/index.js');
-const statsRouteTest = require('./statsRoute.test.js');
-const signupRouteTest = require('./signupRoute.test.js');
+const statsRouteTest = require('./routeTests/statsRoute.test.js');
+const signupRouteTest = require('./routeTests/signupRoute.test.js');
 
 describe('test all routes', () => {
 
+  // all routes are being imported and run here because when
+  // I ran them in individual files I received ECONNREFUSED error
+  // weird downside is that I have to put a dummy test in all the 
+  // route test files (that isn't being exported) or else jest
+  // is unhappy with me
   signupRouteTest(app, server, mongoose, request);
   statsRouteTest(app, server, mongoose, request);
 
-  afterAll((done) => {
+  afterAll(done => {
     // needed for jest to exit properly
     server.close();
     mongoose.connection.close()
