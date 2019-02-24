1d2ecc66ba70efc2ca09761480c0c568f66c807b
Hannah Mitchell <hannah@hannahs-mbp-2.home>
2019-02-24 08:43:22 -0800

    internet restored

--- src/server/index.js
+++ src/server/index.js
@@ -30,11 +30,10 @@ app.use('/stats', statsRouter);
 app.use('/*', (req, res) => res.send('404 not found'));
 app.use((err, req, res, next) => res.send(err.message));
 
-// database connection (no internet right now)
-// mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, (err) => {
-//   if (err) console.log(err);
-//   else console.log('Connected to db');
-// })
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, (err) => {
+  if (err) console.log(err);
+  else console.log('Connected to db');
+})
 
 // server connection
 const server = app.listen(process.env.PORT || 3000, (err) => {
